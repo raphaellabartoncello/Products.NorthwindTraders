1973621006490528d5e1c5748b5a4e24621e5114
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-06-17 20:21:48 -0300

    Definir caminhos da model para o banco

--- Products.NorthwindTraders/Models/Categoria.cs
+++ Products.NorthwindTraders/Models/Categoria.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Products.NorthwindTraders.Models
 {
+    [Table("Categories")]
     public class Categoria
     {
+        [Column("CategoryId")]
         public int CategoriaId { get; set; }
+        [Column("CategoryName")]
         public string Nome { get; set; }
     }
 }
--- Products.NorthwindTraders/Models/Produto.cs
+++ Products.NorthwindTraders/Models/Produto.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Products.NorthwindTraders.Models
 {
+    [Table("Products")]
     public class Produto
     {
+        [Column("ProductId")]
+        public int ProdutoId { get; set; }
+        [Column("ProductName")]
+        public string Nome { get; set; }
+        [Column("UnitPrice")]
+        public decimal Preco { get; set; }
+        [Column("UnitsInStock")]
+        public short Estoque { get; set; }
+        [ForeignKey("Categoria")]
+        [Column("CategoryId")]
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
     }
 }
