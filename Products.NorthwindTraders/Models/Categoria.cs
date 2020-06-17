b2c03a3f28fbf5ec5eb7ef8f8aa85270515880fd
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-06-17 20:19:16 -0300

    Adicionar pasta Model e classes categoria e produto

---
+++ Products.NorthwindTraders/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Products.NorthwindTraders.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string Nome { get; set; }
+    }
+}
