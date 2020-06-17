3e49ac01fcbf7ea152be201c5907b830796cd0e5
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-06-17 20:31:04 -0300

    Criar métodos de categoria e produto

---
+++ Products.NorthwindTraders/DAL/CategoriaDb.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Products.NorthwindTraders.Models;
+
+namespace Products.NorthwindTraders.DAL
+{
+    public class CategoriaDb
+    {
+        // CategoriasLista
+        // Retorna a lista de Categorias
+        //
+        //
+        public List<Categoria> CategoriasLista()
+        {
+            using (var db = new NorthwindContext())
+            {
+                return db.Categorias.ToList();
+            }
+        }
+    }
+
+    //Produtos por Categoria
+    // retorna uma lista de produtos de uma categoria
+    //
+    public List<Produto> ProdutosPorCategoria(int categoriaId)
+    {
+        using (var db = new NorthwindContext())
+        {
+            var query = from c in db.Produtos
+                        where c.CategoriaId == categoriaId
+                        select c;
+            var lista = query.ToList();
+
+            return lista;
+        }
+    }
+
+    // CategoriaObter
+    // retorna uma categoria a partir do Id
+    //
+    public Categoria CategoriaObter(int id)
+    {
+        using (var db = new NorthwindContext())
+        {
+            return db.Categorias
+           .Where(m => m.CategoriaId == id).FirstOrDefault();
+        }
+    }
+}
---
+++ Products.NorthwindTraders/DAL/NorthwindContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Products.NorthwindTraders.Models;
+
+namespace Products.NorthwindTraders.DAL
+{
+    public class NorthwindContext : DbContext
+    {
+        private const string Conexao = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public NorthwindContext() : base(Conexao) { }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+    }
+}
